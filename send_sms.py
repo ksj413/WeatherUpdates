9501f00ec4f4bb8ac67b5e2d09954fca314789f4
Ketaki Joshi <KetakiJoshi@Ketakis-MacBook-Pro-2.local>
2018-02-19 14:30:46 -0800

    Adding scheduling

--- send_sms.py
+++ send_sms.py
@@ -2,20 +2,30 @@
 from twilio.rest import Client
 import os
 import requests
+import schedule
+import time
 
-r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Seattle&APPID='+os.environ["OPENWEATHER_API_KEY"]+'&units=imperial')
-result = r.json()
 
-weather_info = result['weather'][0]
-temp_info = result['main']
+def send():
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Seattle&APPID='+os.environ["OPENWEATHER_API_KEY"]+'&units=imperial')
+    result = r.json()
 
-account_sid=os.environ["TWILIO_ACCOUNT_SID"]
-account_auth_token=os.environ["TWILIO_AUTH_TOKEN"]
+    weather_info = result['weather'][0]
+    temp_info = result['main']
 
-client=Client(account_sid, account_auth_token)
+    account_sid=os.environ["TWILIO_ACCOUNT_SID"]
+    account_auth_token=os.environ["TWILIO_AUTH_TOKEN"]
 
-client.messages.create(
-    to=os.environ["MY_TWILIO_NUMBER"],
-    from_=os.environ["TWILIO_NUMBER"],
-    body="Today's weather in Seattle \n {} \n Details: {} \n Temperature: {}(min) - {}(max)".format(weather_info['main'], weather_info['description'], temp_info['temp_min'], temp_info['temp_max'])
-)
+    client=Client(account_sid, account_auth_token)
+
+    client.messages.create(
+        to=os.environ["MY_TWILIO_NUMBER"],
+        from_=os.environ["TWILIO_NUMBER"],
+        body="Today's weather in Seattle \n {} \n Details: {} \n Temperature: {}(min) - {}(max)".format(weather_info['main'], weather_info['description'], temp_info['temp_min'], temp_info['temp_max'])
+    )
+
+schedule.every().day.at("08:00").do(send)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
